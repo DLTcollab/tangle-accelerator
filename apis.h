59e358954b522fd974de516883e512b6a9fc4eef
Yu Wei Wu <wusyong9104@gmail.com>
2019-01-11 19:47:40 +0800

    Implement api_get_tips_pair

--- apis.h
+++ apis.h
@@ -13,6 +13,8 @@ int handle_request(const iota_client_service_t* const service,
                    const char* const obj);
 int api_generate_address(const iota_client_service_t* const service,
                          char** json_result);
+int api_get_tips_pair(const iota_client_service_t* const service,
+                      char** json_result);
 int api_get_tips(const iota_client_service_t* const service,
                  char** json_result);
 int api_send_transfer(const iota_client_service_t* const service,
