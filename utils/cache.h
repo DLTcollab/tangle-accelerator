c792f0367685c343fadf9681e8300aa9076f2223
Yu Wei Wu <wusyong9104@gmail.com>
2019-02-19 14:42:06 +0800

    Documentation for utils

--- utils/cache.h
+++ utils/cache.h
@@ -7,45 +7,72 @@
 
 #include "types/types.h"
 
+/**
+ * @file cache.h
+ * @brief Implementation of cache interface
+ * @example test_cache.c
+ */
+
+/** struct of cache_t */
 typedef struct {
+  /** @cond private data */
   void* conn;
+  /** @endcond */
 } cache_t;
 
-/** Initiate cache module */
+/**
+ * Initiate cache module
+ *
+ * @return
+ * - struct of cache_t on success
+ * - NULL on error
+ */
 cache_t* cache_init();
 
-/** Stop interacting with cache module */
+/**
+ * Stop interacting with cache module
+ *
+ * @param cache Data type for Cache module
+ *
+ * @return NULL
+ */
 void cache_stop(cache_t** cache);
 
 /**
  * Delete certain key-value store from cache
  *
- * @param cache Data type for Cache module
- * @param key Key string to search
+ * @param[in] cache Data type for Cache module
+ * @param[in] key Key string to search
  *
- * @return {int} Status code
+ * @return
+ * - 0 on success
+ * - 1 on error
  */
 int cache_del(const cache_t* const cache, const char* const key);
 
 /**
  * Get key-value store from in-memory cache
  *
- * @param cache Data type for Cache module
- * @param key Key string to search
+ * @param[in] cache Data type for Cache module
+ * @param[in] key Key string to search
+ * @param[out] res Result of GET key
  *
- * @return {string} res Result of GET key
- * @return {int} Status code
+ * @return
+ * - 0 on success
+ * - 1 on error
  */
 int cache_get(const cache_t* const cache, const char* const key, char* res);
 
 /**
  * Set key-value store into in-memory cache
  *
- * @param cache Data type for Cache module
- * @param key Key string to store
- * @param value Value string to store
+ * @param[in] cache Data type for Cache module
+ * @param[in] key Key string to store
+ * @param[in] value Value string to store
  *
- * @return {int} Status code
+ * @return
+ * - 0 on success
+ * - 1 on error
  */
 int cache_set(const cache_t* const cache, const char* const key,
               const char* const value);
