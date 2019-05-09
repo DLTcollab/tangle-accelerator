8d0a85903c540def5d9301b58f75b0e11c20b1fd
YangHau <vulxj0j8j8@gmail.com>
2019-05-09 11:54:07 +0800

    feat(reg): Add DEBUG_FLAG for debuggin mode

--- tests/regression/runner.py
+++ tests/regression/runner.py
@@ -7,8 +7,14 @@ import unittest
 import statistics
 import time
 
+DEBUG_FLAG = False
 if len(sys.argv) == 2:
     raw_url = sys.argv[1]
+elif len(sys.argv) == 3:
+    raw_url = sys.argv[1]
+    # if DEBUG_FLAG field == `Y`, then starts debugging mode
+    if sys.argv[2] == 'y' or sys.argv[2] == 'Y':
+        DEBUG_FLAG = True
 else:
     raw_url = "localhost:8000"
 url = "http://" + raw_url
@@ -19,7 +25,10 @@ TIMEOUT = 100  # [sec]
 MSG_STATUS_CODE_405 = "[405] Method Not Allowed"
 CURL_EMPTY_REPLY = "000"
 
-TIMES_TOTAL = 100
+if DEBUG_FLAG == True:
+    TIMES_TOTAL = 2
+else:
+    TIMES_TOTAL = 100
 
 
 def eval_stat(time_cost, func_name):
