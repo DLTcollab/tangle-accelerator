ba7588830f20f5a89aa8b55e13579b49f708c8a3
Yu Wei Wu <wusyong9104@gmail.com>
2019-06-19 00:57:09 +0800

    feat(map): Implement mam wrapper
    Move basic mam api into wrapper called map/mode. The wrapper is also moved
    under the map directory which is stand for Mask Authenticated Privacy. It
    will be a seperate binary to implement mam utility in the future.

---
+++ map/mode.h
@@ -0,0 +1,98 @@
+#ifndef __MAP_MODE_H__
+#define __MAP_MODE_H__
+
+#include "mam/api/api.h"
+
+#define MSS_DEPTH 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Creates a channel
+ *
+ * @param api - The API [in,out]
+ * @param channel_id - A known channel ID [out]
+ *
+ * @return return code
+ */
+retcode_t map_channel_create(mam_api_t* const api, tryte_t* const channel_id);
+
+/**
+ * Creates and announce a new channel on header
+ *
+ * @param api - The API [in,out]
+ * @param channel_id - A known channel ID [in]
+ * @param bundle - The bundle that the packet will be written into [out]
+ * @param msg_id - The msg_id [out]
+ * @param new_channel_id - The new channel ID [out]
+ *
+ * @return return code
+ */
+retcode_t map_announce_channel(mam_api_t* const api, tryte_t const* const channel_id,
+                               bundle_transactions_t* const bundle, trit_t* const msg_id,
+                               tryte_t* const new_channel_id);
+
+/**
+ * Creates and announce a new endpoint on header
+ *
+ * @param api - The API [in,out]
+ * @param channel_id - A known channel ID [in]
+ * @param bundle - The bundle that the packet will be written into [out]
+ * @param msg_id - The msg_id [out]
+ * @param new_endpoint - The new endpoint [out]
+ *
+ * @return return code
+ */
+retcode_t map_announce_endpoint(mam_api_t* const api, tryte_t const* const channel_id,
+                                bundle_transactions_t* const bundle, trit_t* const msg_id,
+                                tryte_t* const new_endpoint_id);
+
+/**
+ * Writes a header only bundle on a channel
+ *
+ * @param api - The API [in,out]
+ * @param channel_id - A known channel ID [in]
+ * @param bundle - The bundle that the packet will be written into [out]
+ * @param msg_id - The msg_id [out]
+ *
+ * @return return code
+ */
+retcode_t map_write_header_on_channel(mam_api_t* const api, tryte_t const* const channel_id,
+                                      bundle_transactions_t* const bundle, trit_t* const msg_id);
+
+/**
+ * Writes a header only bundle on an endpoint
+ *
+ * @param api - The API [in,out]
+ * @param channel_id - A known channel ID [in]
+ * @param endpoint_id - A known endpoint ID [in]
+ * @param bundle - The bundle that the packet will be written into [out]
+ * @param msg_id - The msg_id [out]
+ *
+ * @return return code
+ */
+retcode_t map_write_header_on_endpoint(mam_api_t* const api, tryte_t const* const channel_id,
+                                       tryte_t const* const endpoint_id, bundle_transactions_t* const bundle,
+                                       trit_t* const msg_id);
+
+/**
+ * Writes a packet on a bundle
+ *
+ * @param api - The API [in,out]
+ * @param bundle - The bundle that the packet will be written into [out]
+ * @param payload - The payload to write [in]
+ * @param msg_id - The msg_id [in]
+ * @param is_last_packet - True if this is the last packet to be written [in]
+ *
+ * @return return code
+ */
+retcode_t map_write_packet(mam_api_t* const api, bundle_transactions_t* const bundle, char const* const payload,
+                           trit_t const* const msg_id, bool is_last_packet);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // __MAP_MODE_H__
