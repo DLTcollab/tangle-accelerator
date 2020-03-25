b78ac00bf0625df70aabeab71c46af8850096f88
yinghan <locke2833@gmail.com>
2020-03-26 01:08:02 +0800

    Remove unused old permanode implementation

--- storage/ta_storage.h
+++ storage/ta_storage.h
@@ -12,7 +12,6 @@ extern "C" {
 #endif
 #include "scylladb_chronicle.h"
 #include "scylladb_identity.h"
-#include "scylladb_permanode.h"
 #include "scylladb_thread_pool.h"
 /**
  * @file storage/ta_storage.h
