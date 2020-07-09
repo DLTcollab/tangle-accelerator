6a7b57377d7fefe27ea7c3b308c35057d3ff9144
HYChang <hychang@biilabs.io>
2020-07-09 10:24:31 +0800

    Fix(endpoint): Fix device not found error message
    The endpoint will always get specific device during compile-time.
    This commit fixed the error message which generated from ta_device.

--- endpoint/endpointComp/endpoint.c
+++ endpoint/endpointComp/endpoint.c
@@ -126,21 +126,25 @@ COMPONENT_INIT {
   srand(time(NULL));
 
   device_t* device = ta_device(STRINGIZE(EP_TARGET));
-  device->op->get_key(private_key);
-  device->op->get_device_id(device_id);
-
+  if (device == NULL) {
+    LE_ERROR("Can not get specific device");
+  } else {
+    device->op->get_key(private_key);
+    device->op->get_device_id(device_id);
 #ifdef ENABLE_ENDPOINT_TEST
-  LE_TEST_INIT;
-  LE_TEST_INFO("=== ENDPOINT TEST BEGIN ===");
-  LE_TEST(SC_OK == send_transaction_information(host, port, ssl_seed, value, message, message_fmt, tag, address,
-                                                next_address, private_key, device_id_ptr, iv));
-  LE_TEST_EXIT;
+    LE_TEST_INIT;
+    LE_TEST_INFO("=== ENDPOINT TEST BEGIN ===");
+    LE_TEST(SC_OK == send_transaction_information(host, port, ssl_seed, value, message, message_fmt, tag, address,
+                                                  next_address, private_key, device_id_ptr, iv));
+    LE_TEST_EXIT;
 #else
-  while (true) {
-    status_t ret = send_transaction_information(host, port, ssl_seed, value, message, message_fmt, tag, address,
-                                                next_address, private_key, device_id_ptr, iv);
-    LE_INFO("Send transaction information return: %d", ret);
-    sleep(10);
-  }
+    while (true) {
+      // TODO: listen input from UART here
+      status_t ret = send_transaction_information(host, port, ssl_seed, value, message, message_fmt, tag, address,
+                                                  next_address, private_key, device_id_ptr, iv);
+      LE_INFO("Send transaction information return: %d", ret);
+      sleep(10);
+    }
 #endif
+  }
 }
